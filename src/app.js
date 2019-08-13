648d56e9095566925bf0219dc3f0ba1cfb3a21f7
Ofry Linkovsky <15746116+OfryL@users.noreply.github.com>
2019-08-13 15:51:25 +0300

    adding bot

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const Telegraf = require('telegraf');
+const config = require('config');
+const parse = require('./msgParser');
+const START_MSG = "Plz forward ur missing staff msg and see the magic!";
+
+const bot = new Telegraf(config.get('token'));
+
+bot.start((ctx) => handleStartCmd(ctx));
+bot.on("forward" ,(ctx) => handleForward(ctx));
+
+function handleStartCmd(ctx) {
+	console.log(`Start from user: ${ctx.message.from.first_name}(@${ctx.message.from.username})`);
+	ctx.telegram.sendMessage(ctx.message.chat.id, START_MSG, {parse_mode:'HTML'} );
+}
+
+function handleForward(ctx) {
+	console.log(`Forward from user: ${ctx.message.from.first_name}(@${ctx.message.from.username})`);
+	let replayData = parse.parseMsg(ctx.message.text);
+	ctx.telegram.sendMessage(ctx.message.chat.id, replayData, {parse_mode:'HTML'} );
+}
+
+bot.startPolling();
