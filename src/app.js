6cfda9e38698b8ec3afdafff89d82b83081f2a43
Ofry Linkovsky <15746116+OfryL@users.noreply.github.com>
2019-11-08 02:34:42 +0200

    adding support for multi line replay

--- src/app.js
+++ src/app.js
@@ -16,8 +16,10 @@ function handleStartCmd(ctx) {
 function handleForward(ctx) {
 	console.log(`Forward from user: ${ctx.message.from.first_name}(@${ctx.message.from.username})`);
 	let replayData = parse.parseMsg(ctx.message.text);
-	if (!replayData) return;  
-	ctx.telegram.sendMessage(ctx.message.chat.id, replayData, {parse_mode:'HTML'} );
+	if (!replayData) return;
+	replayData.forEach(function (line) {
+		ctx.telegram.sendMessage(ctx.message.chat.id, line, {parse_mode:'HTML'} );
+	});
 }
 
 bot.startPolling();
--- src/msgParser.js
+++ src/msgParser.js
@@ -1,20 +1,47 @@
 const resCodes = require('./res.json');
 const resCodesLowerCase = require('./res.lowercase.json');
 const WITHDRAWCMD = "/g_withdraw ";
+const LIMIT = 16;
 module.exports = function() {
   'use strict';
-  
+
+	function getCodes(msg) {
+		let gwithdraw = '',
+		lines = msg.split("\n");
+		lines.forEach(function(element){
+			gwithdraw = gwithdraw + parseLine(element);
+		});
+		return gwithdraw;
+	}
+
+	function splitByLimit(gwithdraw) {
+		let cmd = '', cmds = [], i = 0,lines = gwithdraw.split(WITHDRAWCMD)[1].split(' ');
+		lines.forEach(function (element) {
+			cmd = cmd + ' ' + element;
+			i++;
+			if (i === LIMIT) {
+				cmds.push(cmd);
+				cmd = '';
+				i = 0;
+			}
+		});
+		lines = [];
+		cmds.push(cmd);
+		cmds.forEach(function (element) {
+			let line = WITHDRAWCMD + element;
+			line = line.replace('  ', ' ');
+			lines.push(line);
+		});
+		return lines;
+		// console.log(require('util').inspect(lines));
+	}
+
 	function parseMsg(msg) {
-		let gwithdraw = WITHDRAWCMD,
-			i = 0;
-		while (i < msg.length)
-		{
-			let j = msg.indexOf("\n", i);
-			if (j == -1) j = msg.length;
-			gwithdraw = gwithdraw + parseLine(msg.substr(i, j - i));
-			i = j+1;
-		}
-		if (gwithdraw == WITHDRAWCMD) return;
+		let gwithdraw = WITHDRAWCMD;
+		gwithdraw = gwithdraw + getCodes(msg);
+		if (gwithdraw === WITHDRAWCMD) return;
+		 splitByLimit(gwithdraw);
+		gwithdraw = splitByLimit(gwithdraw);
 		return gwithdraw;
 	}
 	
@@ -34,7 +61,7 @@ module.exports = function() {
 		return resCode;
 	}
 
-	function test() {
+	 const test = function() {
 		console.log(parseMsg(`Not enough materials for crafting Pouch of gold.
 Required:
  9 x Thread`));
@@ -45,7 +72,20 @@ Required:
  7 x Magic stone
  4 x Coal
  24 x Iron ore`));	
-	}
+
+
+		console.log(parseMsg(`Not enough materials. Missing:
+ 1 x Bone
+ 2 x Stick
+ 3 x Magic stone
+ 4 x Magic stone
+ 5 x Magic stone
+ 6 x Magic stone
+ 7 x Magic stone
+ 8 x Magic stone
+ 9 x Coal
+ 10 x Iron ore`));
+	};
 
 	return {
 		parseMsg
